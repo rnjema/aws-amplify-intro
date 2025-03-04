c40026af9e3f15a1b646a245130144bbf2992183
Ramsey Njema <ramseyith99@gmail.com>
2025-03-04 17:30:13 +0200

    Edit app homepage. Include user agent

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1> Hello there, user agent {navigator.userAgent}</h1>
+    </>
+  )
+}
+
+export default App
